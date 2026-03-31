1aa63db1c7b4bf7c4f76ddb9c1b9938f64fce746
Noctus Rex <noxjudex@gmail.com>
2026-03-31 13:21:15 +0200

    no dns

--- src/main.js
+++ src/main.js
@@ -2,7 +2,7 @@ import { Client, Databases } from 'node-appwrite';
 
 export default async ({ req, res, log, error }) => {
   const client = new Client()
-    .setEndpoint(process.env.APPWRITE_FUNCTION_API_ENDPOINT)
+    .setEndpoint("http://172.17.0.1/v1")
     .setProject(process.env.APPWRITE_FUNCTION_PROJECT_ID)
     .setKey(process.env.APPWRITE_API_KEY);
 
